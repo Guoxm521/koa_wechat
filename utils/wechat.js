d80ac3ff7ff6f13e08776bd84d343f27890fcff9
guoxiaoming <guoxiaoming@papa.com.cn>
2023-03-03 16:00:40 +0800

    fix: 🐛 add

--- utils/wechat.js
+++ utils/wechat.js
@@ -1,41 +1,56 @@
-import { XMLParser } from "fast-xml-parser";
+import { XMLParser } from "fast-xml-parser"
+import got from "got"
 import { EventMessage, StandardMessages } from "./handleMessage.js"
+import { wxConfig } from "../config.js"
+import { format } from "util"
 
-// 处理微信公众号的消息 
-async function handleMsg(ctx) {
-    let wechatData = await getWechatData(ctx)
-    const msgXml = Buffer.concat(wechatData).toString('utf-8');
-    const parser = new XMLParser();
-    const { xml } = parser.parse(msgXml);
-    const { MsgType } = xml;
+export class WechatServer {
+  async handleMsg(ctx) {
+    let wechatData = await this.getWechatData(ctx)
+    const msgXml = Buffer.concat(wechatData).toString("utf-8")
+    const parser = new XMLParser()
+    const { xml } = parser.parse(msgXml)
+    const { MsgType } = xml
     let reportMsg = null
-    if (MsgType.toLowerCase() === 'event') {
-        reportMsg = new EventMessage(xml).getMessage()
+    if (MsgType.toLowerCase() === "event") {
+      reportMsg = new EventMessage(xml).getMessage()
     } else {
-        reportMsg = new StandardMessages(xml).getMessage()
+      reportMsg = new StandardMessages(xml).getMessage()
     }
     return reportMsg
+  }
 
-}
-
-// 微信返回的数据为二进制的数据流，因此需要通过监听数据响应的方式获取完整的数据，利用数组做数据缓存
-function getWechatData(ctx) {
+  getWechatData(ctx) {
     return new Promise((resolve, reject) => {
-        try {
-            let postData = []
-            ctx.req.addListener('data', (data) => {
-                postData.push(data)
-            })
-            ctx.req.on('end', () => {
-                resolve(postData)
-            })
-
-        } catch (err) {
-            reject(err)
-        }
+      try {
+        let postData = []
+        ctx.req.addListener("data", (data) => {
+          postData.push(data)
+        })
+        ctx.req.on("end", () => {
+          resolve(postData)
+        })
+      } catch (err) {
+        reject(err)
+      }
     })
+  }
+
+  // 获取微信公众号access_token
+  async getAccessToken() {
+    let url = wxConfig.wxApiURLs.accessTokenURL
+    console.log(url)
+    let { appID, appScrect } = wxConfig
+    url = format(url, appID, appScrect)
+    let res = await got(url, {
+      responseType: "json",
+    })
+    console.log(res.body)
+  }
+  // 创建菜单
+  createMenus() {}
+  // 获取本地素材的media_id
+  getMediaId() {}
 }
 
-
-
-export { getWechatData, handleMsg }
+new WechatServer().getAccessToken()
